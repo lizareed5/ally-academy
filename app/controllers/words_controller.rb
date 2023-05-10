0966df224757560dce7d26352a1b1e5b94328205
Liza McLain <lizamclain@gmail.com>
2023-05-10 16:33:04 -0500

    explanation FE built out

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,7 +2,7 @@ class WordsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :word_not_found
 
     def index
-        render json: Word.all.sort_by(&:word), status: :ok
+        render json: Word.all.sort_by{|word| word.word.downcase}, status: :ok
     end
 
     def show
--- client/src/components/Word.js
+++ client/src/components/Word.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Word({word}) {
 
+    const [showExplanation, setShowExplanation] = useState(false)
+
+    const handleClick = () => {
+        setShowExplanation(!showExplanation)
+    }
+
     const gendernymList = word.gendernyms.map(gendernym =>
-        <h2 className="mb-1">{gendernym}</h2>
+        <div>
+            <h2 className="mb-1 text-lg">{gendernym}</h2>
+            <button onClick={handleClick} className="text-my_blue text-sm mb-2">{showExplanation ? "hide explanation" : "show explanation"}</button>
+            {showExplanation ? <p>explanation here</p> : null}
+        </div>
         )
 
     return (
         <div>
-            <h1 className="text-lg font-semibold mb-1">{word.word}</h1>
+            <h1 className="text-2xl font-semibold mb-1">{word.word}</h1>
             {gendernymList}
         </div>
     )
